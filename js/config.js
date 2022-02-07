d4e54037fa679fb0b3eade5ab8ca1a385cf76913
OliviaKia <grimmgifted@gmail.com>
2022-02-07 14:31:43 +0000

    website config changes

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-2_LZBbgSJUC', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '4lf1gekkc9cc25m2qpq2067mno', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
